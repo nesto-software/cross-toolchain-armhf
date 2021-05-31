7ab9a14f5e9126744414c7ce9c263835845027ee
Martin Löper <martin.loeper@gmx.de>
2021-05-31 21:06:36 +0200

    fix: separate installation from init

--- scripts/install-from-release.sh
+++ scripts/install-from-release.sh
@@ -3,15 +3,14 @@ set -e
 
 TOOLCHAIN=arm-unknown-linux-gnueabi
 TOOLCHAIN_PATH=/opt/crosstool-ng/x-tools/${TOOLCHAIN}/bin
+DOWNLOAD_LOCATION=/tmp/crosstool-ng-armhf.tar.xz
 
-export DOWNLOAD_LOCATION=/tmp/crosstool-ng-armhf.tar.xz
-
-apt-get -qq install -y curl wget xz-utils tar > /dev/null
-
-sudo mkdir -p /opt/crosstool-ng/x-tools/
-
-# only download and install if not already available
+# only prepare, download and install if not already available
 if [ ! -d "/opt/crosstool-ng/x-tools/${TOOLCHAIN}" ]; then
+
+    sudo apt-get -qq install -y curl wget xz-utils tar > /dev/null
+
+    sudo mkdir -p /opt/crosstool-ng/x-tools/
 
     curl -s https://api.github.com/repos/nesto-software/cross-toolchain-armhf/releases/latest \
         | grep "/crosstool-ng-armhf.tar.xz" \
